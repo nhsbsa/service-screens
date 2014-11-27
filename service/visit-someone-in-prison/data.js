dc331381f89cf6d3cae177b4e0b9de74d3b0e816
Mat Harden <matharden@gmail.com>
2014-11-27 10:28:46 +0000

    Add "Booking request" flow for Visit someone in prison

---
+++ service/visit-someone-in-prison/data.js
@@ -0,0 +1,38 @@
+var data = {
+  "service": "Visit someone in prison", // service title
+  "last-updated": "24 November 2014", // date last updated
+  "userjourneys": [
+    {
+      "title": "Booking request", // user journey title
+      "path": [ // array of images
+        {
+          "caption": "Prisoner details", // image caption
+          "imgref": "images/1.prisoner.png" // image path
+        },
+        {
+          "caption": "Visitor details",
+          "imgref": "images/2.visitor.png"
+        },
+        {
+          "caption": "Additional visitors",
+          "imgref": "images/3.visitor_b.png"
+        },
+        {
+          "caption": "Choose a visiting slot",
+          "imgref": "images/4.slots.png"
+        },
+        {
+          "caption": "Choose a visiting slot - chosen states",
+          "imgref": "images/5.slots_b.png"
+        },
+        {
+          "caption": "Check your request",
+          "imgref": "images/6.summary.png"
+        },
+        {
+          "caption": "Request sent",
+          "imgref": "images/7.request_sent.png"
+        }
+      ]
+    }
+  ]}
