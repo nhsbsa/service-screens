a5f7b0a0f08f19038d5d8986f5ef67ebe5565481
Jim Kosem <jim@halfman.com>
2014-12-01 17:13:18 +0000

    Add civil legal advice case handling scenarios

---
+++ service/civil-legal-advice/data.js
@@ -0,0 +1,78 @@
+var data = {
+  "service": "Civil Legal Advice: Case handling", // service title
+  "last-updated": "24 November 2014", // date last updated
+  "userjourneys": [
+    {
+      "title": "Create a case that is in-scope and eligible", // user journey title
+      "path": [ // array of images
+        {
+          "caption": "Cases queue", // image caption
+          "imgref": "images/create-case-inscope-elgible/1.png" // image path
+        },
+        {
+          "caption": "Creating a new case",
+          "imgref": "images/create-case-inscope-elgible/2.png"
+        },
+        {
+          "caption": "Opening a new contact card",
+          "imgref": "images/create-case-inscope-elgible/3.png"
+        },
+        {
+          "caption": "Postcode search and filling in client details",
+          "imgref": "images/create-case-inscope-elgible/4.png"
+        },
+        {
+          "caption": "Starting the assessment",
+          "imgref": "images/create-case-inscope-elgible/5.png"
+        },
+        {
+          "caption": "Scope diagnosis",
+          "imgref": "images/create-case-inscope-elgible/6.png"
+        },
+        {
+          "caption": "Scope complete",
+          "imgref": "images/create-case-inscope-elgible/7.png"
+        },
+        {
+            "caption": "Financial assessement",
+            "imgref": "images/create-case-inscope-elgible/8.png"
+        },
+        {
+            "caption": "Finances",
+            "imgref": "images/create-case-inscope-elgible/9.png"
+        },
+        {
+            "caption": "Assign a specialist provider",
+            "imgref": "images/create-case-inscope-elgible/10.png"
+        }
+      ]
+    }
+    /*{
+      "title": "Out of scope, give alternative help", // user journey title
+      "path": [ // array of images
+        {
+          "caption": "Cases queue", // image caption
+          "imgref": "images/out-of-scope-alternative-help/1.png" // image path
+        },
+        {
+          "caption": "Case out of scope", // image caption
+          "imgref": "images/out-of-scope-alternative-help/2.png" // image path
+        },
+        {
+          "caption": "Assign alternative help", // image caption
+          "imgref": "images/out-of-scope-alternative-help/3.png" // image path
+        },
+      ]
+    }*/
+
+    // in-scope ineligible alternative help
+
+
+
+    // suspend a case
+
+    // schedule a callback
+
+    // search case queue
+
+  ]}
