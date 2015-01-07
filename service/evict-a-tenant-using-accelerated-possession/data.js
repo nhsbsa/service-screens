aa38a25d04a2f5924ec5dab156221e2b40e4f07a
kelliewellingtons <kelliematheson@gmail.com>
2015-01-07 17:17:26 +0000

    Added accelerated claims screenshots

---
+++ service/evict-a-tenant-using-accelerated-possession/data.js
@@ -0,0 +1,46 @@
+var data = {
+  "service": "Evict a tenant using accelerated possession", // service title
+  "last-updated": "7 January 2015", // date last updated
+  "userjourneys": [
+    {
+      "title": "Evict a tenant using accelerated possession", // user journey title
+      "path": [ // array of images
+        {
+          "caption": "Start page", // image caption
+          "imgref": "images/1-start.png" // image path
+        },
+        {
+          "caption": "Form",
+          "imgref": "images/2-form.png"
+        },
+        {
+          "caption": "Claimants module",
+          "imgref": "images/3-claimants.png"
+        },
+        {
+          "caption": "Defendants module",
+          "imgref": "images/4-defendants.png"
+        },
+        {
+          "caption": "Notice module",
+          "imgref": "images/5-notice.png"
+        },
+        {
+          "caption": "Tenancy module",
+          "imgref": "images/6-tenancy.png"
+        },
+        {
+          "caption": "Licences module",
+          "imgref": "images/7-licences.png"
+        },
+        {
+            "caption": "Deposit module",
+            "imgref": "images/8-deposit.png"
+        },
+        {
+            "caption": "Review",
+            "imgref": "images/9-review.png"
+        }
+      ]
+    }
+  ]}
